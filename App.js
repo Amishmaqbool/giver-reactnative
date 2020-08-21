35a50dc30edc7cc7f95d19c8359d2774f604a650
Sassyhamza1 <hamza.iintellect@gmail.com>
2020-08-22 04:24:40 +0500

    Search Contact screen added

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import Dashboard from "./app/screens/Dashboard";
 import { Provider as PaperProvider } from 'react-native-paper';
+import SearchContact from "./app/screens/SearchContact";
 
 
 export default function App() {
   return (
     <PaperProvider >
-  <Dashboard />
+     <SearchContact />
   </PaperProvider>
   );
 }
