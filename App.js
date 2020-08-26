451c1ea5689339c2e9271f7923650bfad13319ef
Amishmaqbool <44437037+Amishmaqbool@users.noreply.github.com>
2020-08-26 18:48:53 +0500

    All screens completed

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Provider as PaperProvider } from 'react-native-paper';
-import PaidSuccessfully from "./app/screens/PaidSuccessfully";
-
+import { Provider as PaperProvider } from "react-native-paper";
+import GiverMain from "./app/screens/GiverMain";
 
 export default function App() {
   return (
-    <PaperProvider >
-     <PaidSuccessfully />
-  </PaperProvider>
+    <PaperProvider>
+      <GiverMain />
+    </PaperProvider>
   );
 }
---
+++ app/screens/GiverMain.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Image, Text, View, StyleSheet, ImageBackground } from "react-native";
+import {Button} from 'react-native-paper'
+
+export default function GiverMain(props) {
+  const image = require("../assets/mainbackground.png");
+
+  return (
+    <View style={styles.container}>
+    <ImageBackground source={image} style={styles.image}>
+      <View style={{flex:1,flexDirection:"row",marginTop:"25%" ,marginHorizontal:"5%"}}>
+      <Image source={require("../assets/icon.png")}></Image>
+      <Text style={styles.text}>Giver</Text>
+      
+       </View>
+       <View  style={{position:"absolute",top:"23%" ,left:"10%"}}>
+       <Text style={{fontSize:28,fontWeight:"bold"}}>Banking in {"\n"}your pocket</Text>
+       </View>
+
+       <View>
+       <Button
+          uppercase={false}
+         
+          style={styles.getstartedbtn}
+          contentStyle={{ height: 50 }}
+          labelStyle={{ color: "#000000", fontSize: 28 }}
+          mode="text"
+          onPress={() => console.log("Pressed")}
+        > Get started</Button>
+       </View>
+    </ImageBackground>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    
+  },
+  text: {
+    marginTop:"2%",
+    color: "#000000",
+    fontSize: 45,
+    fontWeight: "bold"
+  },
+  getstartedbtn: {
+    marginVertical:"25%",
+    marginHorizontal:"15%",
+    width: "70%",
+    borderRadius: 0,
+    backgroundColor:"#FFFFFF",
+    fontSize: 4,
+    fontWeight:"500",
+    elevation:4
+  },
+});
+
+
