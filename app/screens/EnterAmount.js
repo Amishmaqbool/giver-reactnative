fed415bc18a5c0c13e25e4ecf273b1586a5288b2
Amishmaqbool <Amishmaqbool@gmail.com>
2020-09-14 05:42:33 +0500

    call to socket function

--- app/screens/EnterAmount.js
+++ app/screens/EnterAmount.js
@@ -144,7 +144,7 @@ export default function EnterAmount({ navigation }) {
           contentStyle={{ height: 45 }}
           labelStyle={{ color: "white", fontSize: 12 }}
           mode="contained"
-          onPress={() => navigation.navigate('PaidSuccessfully')}
+          onPress={() => AddMoney}
         >
           Pay
         </Button>
